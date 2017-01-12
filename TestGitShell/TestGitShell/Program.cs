78d1cea7500a0aa3311c4dfbef9cb81064ba9687
MMShatalin <mmshatalin@mail.ru>
2017-01-12 14:53:00 +0300

    Добавлено сообщение

--- TestGitShell/TestGitShell/Program.cs
+++ TestGitShell/TestGitShell/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,10 +11,12 @@ namespace TestGitShell
     {
         public Program()
         {
-            
+
         }
+
         static void Main(string[] args)
         {
+            Console.Write("Hi!");
         }
     }
 }
